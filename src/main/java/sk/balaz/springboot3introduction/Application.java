ce9f64e775e361d98b719ffa778ec3ace4ed8aee
Tomas Balaz <balaztomas0@gmail.com>
2023-11-14 14:41:01 +0100

    Exposed REST API GET /greet

--- src/main/java/sk/balaz/springboot3introduction/Application.java
+++ src/main/java/sk/balaz/springboot3introduction/Application.java
@@ -2,12 +2,20 @@ package sk.balaz.springboot3introduction;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@GetMapping()
+	public String greet() {
+		return "Hello";
 	}
 
 }
