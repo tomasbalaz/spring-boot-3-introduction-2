d2aed0a7ece56f19af97d61759d0cf72d08eba00
Tomas Balaz <balaztomas0@gmail.com>
2023-11-22 14:52:50 +0100

    Exposed GET /api/v1/customers

--- src/main/java/sk/balaz/springboot3introduction/Application.java
+++ src/main/java/sk/balaz/springboot3introduction/Application.java
@@ -3,36 +3,28 @@ package sk.balaz.springboot3introduction;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @SpringBootApplication
 @RestController
+@RequestMapping("api/v1/customers")
 public class Application {
+
+	private final CustomerRepository customerRepository;
+
+	public Application(CustomerRepository customerRepository) {
+		this.customerRepository = customerRepository;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
-	@GetMapping()
-	public GreetResponse greet() {
-		GreetResponse response = new GreetResponse(
-				"Hello",
-				List.of("Java", "Golang", "Python"),
-				new Person("Tomas ", 28, 30_000)
-		);
-		return response;
+	@GetMapping
+	public List<Customer> getCustomers() {
+		return customerRepository.findAll();
 	}
-
-	record Person(
-			String name,
-			int age,
-			double savings) {}
-
-	record GreetResponse(
-			String greet,
-			List<String> favouriteProgrammingLanguages,
-			Person person) {}
-
 }
